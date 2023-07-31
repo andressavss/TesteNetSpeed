b8510c0d8a1fb65ed6f207a934ad4281af919734
andressavss <andressavieiradsantos@gmail.com>
2023-07-31 00:06:42 -0300

    Add project files.

---
+++ Teste2/Teste2/Program.cs
@@ -0,0 +1,51 @@
+﻿Console.Write("Código do aluno: ");
+int cod = int.Parse(Console.ReadLine());
+
+while (cod != 0)
+{
+    try
+    {
+        List<double> notes = new List<double>();
+
+        Console.Write("Nota 1 do aluno: ");
+        notes.Add(double.Parse(Console.ReadLine()));
+        Console.Write("Nota 2 do aluno: ");
+        notes.Add(double.Parse(Console.ReadLine()));
+        Console.Write("Nota 3 do aluno: ");
+        notes.Add(double.Parse(Console.ReadLine()));
+
+        int index = 0;
+        double total = 0;
+
+        foreach (var note in notes)
+        {
+            if (note == notes.Max() && index == 0)
+            {
+                total += note * 4;
+                index++;
+            }
+            else
+            {
+                total += note * 3;
+            }
+        }
+
+        double avg = total / 10;
+
+        if (avg >= 6)
+        {
+            Console.WriteLine("Aprovado!");
+        }
+        else
+        {
+            Console.WriteLine("Reprovado!");
+        }
+
+        Console.Write("Código do aluno: ");
+        cod = int.Parse(Console.ReadLine());
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex);
+    }  
+}
---
+++ TesteLogicaNetSpeed/TesteLogicaNetSpeed/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using TesteLogicaNetSpeed;
+
+var listCar = new List<Car>();
+
+string answer = "";
+
+while (answer != "N")
+{
+    try
+    {
+        string question = "Qual o ano do carro? ";
+        Console.Write(question);
+        int year = int.Parse(Console.ReadLine());
+        string question2 = "Qual o valor do carro? ";
+        Console.Write(question2);
+        double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        listCar.Add(new Car()
+        {
+            Year = year,
+            Value = value
+        });
+
+        double discount;
+
+        double total = 0;
+        if (year <= 2000)
+        {
+            discount = value * 0.12;
+            total = value - discount;
+        }
+        else
+        {
+            discount = value * 0.7;
+            total = value - discount;
+        }
+
+        Console.WriteLine($"Valor do desconto: {discount}");
+        Console.WriteLine($"Total à ser pago: {total}");
+
+        Console.Write("Deseja continuar? ");
+        answer = Console.ReadLine().ToUpper();
+    }
+    catch (Exception ex)
+    {
+            Console.WriteLine(ex);
+    }   
+}
+
+Console.WriteLine();
+
+var qtyCarsLess2000 = listCar.Where(sum => sum.Year <= 2000).Count();
+Console.WriteLine($"Quantidade de carros até 2000: {qtyCarsLess2000}");
+Console.WriteLine($"Quantidade de carros: {listCar.Count}");
